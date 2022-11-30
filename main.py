9f63b9356dff6ea3b7c1157b7247c992b6394d3e
Jodis Fields <jodis.fields@outlook.com>
2022-11-30 19:07:33 +1100

    Added main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import networkx as nx
+import plotly.graph_objs as go
+from PIL import Image
+
+
+# Streamlit configuration parameters
+st.set_page_config(page_title="Fission", page_icon="🌐")
+
+
+# Set the title and create file uploader
+st.title("Fission")
+
